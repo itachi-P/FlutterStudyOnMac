22b74c596af6c25ba3938c7cc1ef50492d5de1ce
itachi-P <itachip38@gmail.com>
2020-01-31 13:30:26 +0900

    Create sample App 'myApp02' and running emulator.

---
+++ my_app02/ios/Runner/MyFirstApp/MyFirstApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyFirstApp
+//
+//  Created by Taichi Shinohara on 2020/01/31.
+//  Copyright © 2020 Psycho-Logic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
